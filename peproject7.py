8802bdea8cc3272120ec6aba699f25809f3c0f84
Keaton Galloway <keatongalloway@yahoo.com>
2017-12-20 00:12:55 -0600

    finished project 8

--- peproject7.py
+++ peproject7.py
@@ -11,10 +11,24 @@ def nthprime(n):
     return i - 1
 
 def nthprime2(n):
+    prime_list = [2]
+    i = 3
+    count = 1
+    while count != n:
+        divisible = False
+        for p in prime_list:
+            if i%p == 0:
+                divisible = True
+                break 
+        if not divisible:
+            prime_list.append(i)
+            count +=1
+        i +=1
+    return prime_list[len(prime_list)-1]
 
 def main():
     number = input("nth prime, what is n?")
-    print nthprime(number)
+    print nthprime2(number)
 
 
 if __name__ == '__main__':
