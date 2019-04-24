a09e0e44d0e7e55e87f26e973296c73b879f507b
Keaton Galloway <keatongalloway@yahoo.com>
2019-04-24 17:30:26 -0500

    Mosty learned about copying files

---
+++ ex17v2.py
@@ -0,0 +1,10 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Copying from %s to %s" % (from_file, to_file)
+
+in_file = open(from_file)
+out_file = open(to_file, 'w')
+out_file.write (in_file.read())
